c53a2c491bda9c786531cd1b720911e4c7a023ed
MChenFK <michaelchen1923@gmail.com>
2025-02-12 06:29:56 -0500

    added draw_move function for movement lines between cells

--- src/cell.py
+++ src/cell.py
@@ -14,6 +14,9 @@ class Cell:
         self._win = win
     
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -31,3 +34,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half1 = abs(self._x2 - self._x1) // 2
+        center1 = Point(self._x1 + half1, self._y1 + half1)
+
+        half2 = abs(to_cell._x2 - to_cell._x1) // 2
+        center2 = Point(to_cell._x1 + half2, to_cell._y1 + half2)
+
+        line = Line(center1, center2)
+
+        if not undo:
+            line_color = "red"
+        else:
+            line_color = "gray"
+
+        self._win.draw_line(line, line_color)
