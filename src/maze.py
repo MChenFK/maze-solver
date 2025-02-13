a2336b05507d18ee9d0ff8c7ad39f026daa5e688
MChenFK <michaelchen1923@gmail.com>
2025-02-12 21:28:13 -0500

    added recursive solve function

--- src/maze.py
+++ src/maze.py
@@ -110,3 +110,41 @@ class Maze:
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._cells[i][j].visited = False
+
+    def solve(self):
+        return self._solve_r(0, 0)
+    
+    def _solve_r(self, i, j):
+        self._animate()
+        self._cells[i][j].visited = True
+        if i == self._num_cols - 1 and j == self._num_rows - 1:
+            return True
+        if i > 0 and not self._cells[i][j].has_left_wall and not self._cells[i - 1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i - 1][j])
+            solved = self._solve_r(i - 1, j)
+            if solved:
+                return True
+            self._cells[i][j].draw_move(self._cells[i - 1][j], undo=True)
+
+        if i < self._num_cols - 1 and not self._cells[i][j].has_right_wall and not self._cells[i + 1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i + 1][j])
+            solved = self._solve_r(i + 1, j)
+            if solved:
+                return True
+            self._cells[i][j].draw_move(self._cells[i + 1][j], undo=True)
+
+        if j > 0 and not self._cells[i][j].has_top_wall and not self._cells[i][j - 1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j - 1])
+            solved = self._solve_r(i, j - 1)
+            if solved:
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j - 1], undo=True)
+
+        if j < self._num_rows - 1 and not self._cells[i][j].has_bottom_wall and not self._cells[i][j + 1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j + 1])
+            solved = self._solve_r(i, j + 1)
+            if solved:
+                return True
+            self._cells[i][j].draw_move(self._cells[i][j + 1], undo=True)
+
+        return False
